8734a1757c10c9b5d54fa3132b40d0b8bb87ec6b
Thomas CASTELLANO <91903714+ThomasCastellano@users.noreply.github.com>
2021-10-29 17:17:40 +0200

    Add files via upload

--- orex_web_app.py
+++ orex_web_app.py
@@ -1,17 +1,37 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/order', methods = ["GET", "POST"])  
 def index():
+	if request.method == "POST":
+		#chassis = request.form.get("f_chassis")				# Recupere type chassis depuis formulaire
+		#carosserie = request.form.get("f_carosserie")		# idem pour carosserie
+		#option = request.form.get
+		
+		chassis = request.form.get("chassis")
+		carosserie = request.form.get("carosserie")
+		option = 
+		
+		print("Copyright Thomas CASTELLANO. All rights reserved.")
+		
+		"""
+		if request.form.get("chassis"):
+			chassis = 'c'
+		else :
+			chassis = 'l'
+		
+		if request.form.get("carosserie_o"):
+			carosserie = 'o'
+		else :
+			carosserie = 'f'
+		
+		# pour les options : spécifié le type d'envoie dans le form
+		"""
+		
+	return render_template('order.html')
 	
-	contenu = ""
-	contenu = "<h1/>Agi[lean|log] Web-app</h1>"
-	contenu += "<br/>"
-	contenu += "<a href='/reset_db'>Réinitialiser la base de données</a>"
-	
-	return contenu;
-	
+# Supprimer OREX-WEB.db et le remplacer par OREX-WEB_original.db (après l'avoir renommé)
 @app.route('/reset_db')
 def reset_db():
 	contenu = ""
@@ -20,7 +40,7 @@ def reset_db():
 	
 @app.route('/view_db')
 def lire_db():
-	contenur = ""
+	contenu = ""
 	
 	return contenu
 	
